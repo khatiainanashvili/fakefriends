61201962e27620cf051bdcf53b4632dcda6fa01a
khatiainanashvili <118260235+khatiainanashvili@users.noreply.github.com>
2025-08-08 23:20:53 +0400

    Update realMemories.tsx

--- src/realMemories.tsx
+++ src/realMemories.tsx
@@ -8,7 +8,7 @@ const realImages = [
   "images/real/real5.mov",
   "images/real/real6.MOV",
   "images/real/real7.mp4",
-  "images/real/real8.MOV",
+  "images/real8.mov",
 ];
 
 const RealMemories = () => {
